5616ccbaf81f01b20ebd999976b594bcc316cb95
Yoshiyuki Koyanagi <moutend@gmail.com>
2017-01-05 19:00:40 +0900

    Bump up version v0.3.0

--- gip.rb
+++ gip.rb
@@ -1,16 +1,16 @@
 class Gip < Formula
   desc 'CLI tool for displaying GitHub Issues and Pull requests'
   homepage 'https://github.com/moutend/gip'
-  version 'v0.1.1'
+  version 'v0.3.0'
 
   if Hardware::CPU.is_64_bit?
-    @@binname = 'gip_darwin_amd64_v0.1.1'
-    url 'https://github.com/moutend/gip/releases/download/v0.1.1/' << @@binname
-    sha256 '7696c0143c6a1acbc2692321a461229db19ade5f13c83512074747ac08dadfb6'
+    @@binname = 'gip_darwin_amd64_v0.3.0'
+    url 'https://github.com/moutend/gip/releases/download/v0.3.0/' << @@binname
+    sha256 '5ee1573d01959547b0d6860a04b0f896f3716c89acb3122d44cdc7a91acc5bec'
   else
-    @@binname = 'gip_darwin_386_v0.1.1'
-    url 'https://github.com/moutend/gip/releases/download/v0.1.1/' << @@binname
-    sha256 '294853725187c99592abfed4b9497a150fbd599cd81f6a6230b8917337e73a72'
+    @@binname = 'gip_darwin_386_v0.3.0'
+    url 'https://github.com/moutend/gip/releases/download/v0.3.0/' << @@binname
+    sha256 '3464b6fe1d5ed13f659441ac691207e62ec344c8d52eb5098f2a18a46b21324c'
   end
 
   def install
